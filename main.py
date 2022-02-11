9f054b028b2961b70a3811995cfe668a8caaa299
wagaojr <wagnerjunior@poli.ufrj.br>
2022-02-11 18:12:06 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st;
+
+st.title("ProMove")
+
+with st.form(key="include_cliente"):
+    input_name = st.text_input(label="Insira seu nome")
+    input_age = st.number_input(label="Insira sua idade", format="%d", step=1)
+    input_occupation = st.selectbox("Qual o seu vínculo com a UFRJ?",["Estudante","Professor","Servidor","Pesquisador CLT","Funcionário","Ex Aluno"])
+    input_local = st.selectbox("Onde trabalha/estuda na UFRJ?",["CCMN","CT","Letras","CCS"])
+    input_pergunta1 = st.selectbox("Você sente dificuldade nos estudos?", ["Sim","Não"])
+    input_pergunta2 = st.selectbox("Usaria um aplicativo específico da UFRJ para estudos?", ["Sim","Não"])
+    input_button_submit = st.form_submit_button("Enviar")
+
+
+if input_button_submit:
+    pass
---
+++ serviços/database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+
+class BancodeDados:
+    def __init__(self):
+        self.conexao = sqlite3.connect("bancoDados.db")
+        self.createTable()
+
+    def createTable(self):
+        c = self.conexao.cursor()
+        #Cria uma tabela
+        c.execute(
+            """
+            create table if not exists dados(
+                id integer primary key autoincrement,
+                nome text,
+                idade integer,
+                local text,
+                pergunta1 text,
+                pergunta2 text
+            )
+            """
+        )
+        self.conexao.commit()
+        c.close()
